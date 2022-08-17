881e601b30ef73fb972ca44e9eb70d5751fbea4b
Charles Anderson <master.sparkle@gmail.com>
2022-08-17 15:16:40 -0700

    Add real-ish support for PRINT command - hello world

--- pkg/statement.go
+++ pkg/statement.go
@@ -2,6 +2,7 @@ package gobasic
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -15,6 +16,7 @@ type Statement interface {
 // for the moment, statements are not polymorphic. We can add that later befind the Statement interface
 type statement struct {
 	lineNo int
+	opCode string
 	text   string
 }
 
@@ -23,8 +25,9 @@ const NEXT_LINE = -1
 func ParseStatement(line string) (Statement, error) {
 	toks := strings.Fields(line)
 	lineNo, _ := strconv.Atoi(toks[0]) // ATM we assume all digits already
+	opCode := strings.ToUpper(toks[1])
 
-	result := statement{lineNo, line}
+	result := statement{lineNo, opCode, line}
 
 	return result, nil
 }
@@ -32,6 +35,17 @@ func ParseStatement(line string) (Statement, error) {
 func (s statement) Execute() (int, error) {
 	fmt.Printf("executing: %s\n", s.text)
 	// switch on type of statement b/c we're not doing polymorphism, yet
+	switch s.opCode {
+	case "PRINT":
+		// gross re-parsing - extract every after PRINT
+		re := regexp.MustCompile((`(?i)^.*print `))
+		msg := re.ReplaceAllString(s.text, "")
+		fmt.Println(msg)
+	default:
+		fmt.Println("unrecognized op code - shouldn't happen")
+		//throw an error
+	}
+
 	return NEXT_LINE, nil
 }
 
